76d54bb9e3794a2787efa25b3df62742b9c2db59
Avizinis <mariuz77@gmail.com>
2023-08-26 16:58:08 +0300

    Added tasks[] to local storage

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,8 @@ type Task = {
 const list = document.querySelector<HTMLUListElement>("#list")
 const form = document.getElementById("new_task_form") as HTMLFormElement | null
 const input = document.querySelector<HTMLInputElement>("#new_task_title")
-const tasks: Task[] = []
+const tasks: Task[] = loadTasks()
+tasks.forEach(addListItem)
 
 // add event listener to the submit event of the form. 
 form?.addEventListener("submit", e => {
@@ -27,6 +28,7 @@ form?.addEventListener("submit", e => {
     }
 
     tasks.push(newTask)
+    saveTasks()
 
     addListItem(newTask)
     input.value = ""
@@ -39,10 +41,21 @@ function addListItem(task: Task) {
     checkbox.addEventListener("change", e => {
         task.completed = checkbox.checked
         console.log(tasks)
+        saveTasks()
     })
     checkbox.type = "checkbox"
     checkbox.checked = task.completed
     label.append(checkbox, task.title)
     item.append(label)
     list?.append(item)
+}
+
+function saveTasks() {
+    localStorage.setItem("TASKS", JSON.stringify(tasks))
+}
+
+function loadTasks(): Task[] {
+    const taskJSON = localStorage.getItem("TASKS")
+    if(taskJSON == null) return []
+    return JSON.parse(taskJSON)
 }
